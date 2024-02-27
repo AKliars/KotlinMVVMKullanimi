cd49f64329eb5f12e1b8d1c8d9f68e8e594cac66
Ali KARS <alikars41@gmail.com>
2024-02-27 21:04:27 +0300

    DataBinding işlendi. ModelView, Repository, LiveData, DataSource gibi kavramlara değinildi.

---
+++ app/src/main/java/com/akliars/mvvmkullanimi/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.akliars.mvvmkullanimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.akliars.mvvmkullanimi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityMainBinding
+    private val viewModel:MainActivityViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.mainActivityNesnesi = this
+
+        viewModel.sonuc.observe(this){
+            binding.hesaplamaSonucu = it
+        }
+    }
+
+    fun topla(alinanSayi1:String,alinanSayi2:String){
+        viewModel.topla(alinanSayi1,alinanSayi2)//İşlem Yapıyor
+    }
+    fun carp(alinanSayi1:String,alinanSayi2:String){
+        viewModel.carp(alinanSayi1,alinanSayi2)
+    }
+}
